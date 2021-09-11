9fadc4cfad99c920543f9cfadc3924f743fc2e6f
Jacques857 <jleongsi@ualberta.ca>
2021-09-11 13:53:27 -0600

    fixed conflict

--- health_tracker/health_tracker/settings.py
+++ health_tracker/health_tracker/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'health_tracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'health_tracker/base_app/templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
