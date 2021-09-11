3e3a98a7ab29413a5b3849144a741151be96a083
Will Fenton <wfenton@protonmail.com>
2021-09-11 11:46:39 -0600

    Move accounts stuff to baseapp

--- health_tracker/base_app/views.py
+++ health_tracker/base_app/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+from .forms import UserCreationForm
 
 
 def index(request):
@@ -6,8 +8,36 @@ def index(request):
 
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        # match credentials to user
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth_login(request, user)  # log user in
+            return redirect('profile')  # redirect to profile - get name/url of page to redirect to
+        else:
+            message.error(request, 'Username OR Password is incorrect')
+
+    return render(request, 'login.html')  # failure to log in
 
 
 def register(request):
-    return render(request, 'register.html')
+    if request.method == 'POST':
+        # create user
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+
+            # login and authenticate user
+            return redirect('login')  # redirect to profile - get name/url of page to redirect to
+
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+
+def logout(request):
+    auth_logout(request)
+    return redirect('login')  # redirect to profile - get name/url of page to redirect to
--- health_tracker/health_tracker/settings.py
+++ health_tracker/health_tracker/settings.py
@@ -29,7 +29,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'accounts.apps.AccountsConfig',
     'base_app',
     'django.contrib.admin',
     'django.contrib.auth',
