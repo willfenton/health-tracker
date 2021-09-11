4b2b95507ac7043fc1b784b79321b694613e7d84
Tristen Glover <tmg098@gmail.com>
2021-09-11 10:02:56 -0600

    EVENT MODLE: add initial schema for event model

--- health_tracker/base_app/models.py
+++ health_tracker/base_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.db.models.fields import CharField
+from django.utils import timezone
+from django.contrib.auth.models import User
+import datetime
 
 # Create your models here.
+class Events(models.Model):
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
+    activity_date = models.DateTimeField("activity date")
+    activity = models.CharField(max_length=200)
+    points = models.IntegerField(default=0)
+   
