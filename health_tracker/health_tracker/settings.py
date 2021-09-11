35b81f65bde968d7734bf4c096122b9f5e6b94b6
Will Fenton <wfenton@protonmail.com>
2021-09-11 10:25:30 -0600

    Basic HTML + Bootstrap

--- health_tracker/health_tracker/settings.py
+++ health_tracker/health_tracker/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'base_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,9 @@ ROOT_URLCONF = 'health_tracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'health_tracker/base_app/templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
