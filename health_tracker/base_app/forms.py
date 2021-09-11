f1ed520354845b11c482395999778c75830a64a5
Will Fenton <wfenton@protonmail.com>
2021-09-11 13:50:25 -0600

    Account stuff working! 🎊

--- health_tracker/base_app/forms.py
+++ health_tracker/base_app/forms.py
@@ -1,8 +1,18 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
 
 
-class CreateUserForm(UserCreationForm):
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'username', 'email', 'password', ]
+        fields = ("username", "email", "password1", "password2")
+
+    def save(self, commit=True):
+        user = super(RegisterForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
--- health_tracker/base_app/urls.py
+++ health_tracker/base_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'base_app'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login', views.login, name='login'),
+    path('login', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout', auth_views.LogoutView.as_view(next_page='base_app:login'), name='logout'),
     path('register', views.register, name='register'),
-    path('logout', views.logout, name='logout'),
 ]
