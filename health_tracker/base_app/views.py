59941e0b3af49d18e57ac6fe9cc03ae951789424
Jacques857 <jleongsi@ualberta.ca>
2021-09-11 13:37:05 -0600

    TrackerUser is being serialized properly, but not events

--- health_tracker/base_app/views.py
+++ health_tracker/base_app/views.py
@@ -2,7 +2,19 @@ from django.contrib import messages
 from django.contrib.auth import login
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+from .forms import UserCreationForm
+from rest_framework import viewsets
+from .serializers import TrackerUserSerializer, EventsSerializer
+from .models import TrackerUser, Events
 
+class TrackerUserViewSet(viewsets.ModelViewSet):
+    queryset = TrackerUser.objects.all().order_by('id')
+    serializer_class = TrackerUserSerializer
+
+class EventViewSet(viewsets.ModelViewSet):
+    queryset = Events.objects.all().order_by('userId')
+    serializer_class = EventsSerializer
 
 def index(request):
     return render(request, 'index.html')
